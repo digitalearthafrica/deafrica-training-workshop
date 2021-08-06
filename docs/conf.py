d21ac40984c28e7bfb3d02233ffbd857d2f4d922
Andrew Hicks <ahicks@frontiersi.com.au>
2021-08-06 17:55:00 +1000

    But not that setting

--- docs/conf.py
+++ docs/conf.py
@@ -88,7 +88,7 @@ html_context = {
 
 
 # Translation options
-gettext_uuid = False
+gettext_uuid = True
 gettext_compact = "docs"  # makes a single "docs.po" file
 gettext_location = False  # location seems to cause issues
 locale_dirs = ['locales/']
